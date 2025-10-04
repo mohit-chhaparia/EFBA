a413d4b3c2df461f9b38a418ce9068662c86179b
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2025-10-03 22:36:25 -0500

    Added visible global definitions for functions used by internal functions like hw().

---
+++ R/imports.R
@@ -0,0 +1,11 @@
+#' Internal import
+#'
+#' These imports make base plotting and IO functions visible to the package,
+#' removing "no visible global function definition" NOTES.
+#'
+#' @keywords internal
+#'
+#' @importFrom graphics abline par plot.new
+#' @importFrom grDevices pdf dev.off gray rgb
+#' @importFrom utils read.csv
+NULL
