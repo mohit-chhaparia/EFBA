09d61cce0dd1745713dcbf951bede908a1d45715
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2025-10-03 23:51:53 -0500

    Combined EFBA-package.R and imports.R file to keep one common package level internal imports files titled EFBA-package.R.

--- R/EFBA-package.R
+++ R/EFBA-package.R
@@ -1,8 +1,13 @@
 #' EFBA: Empirical Frequency Band Analysis
- #'
- #' Tools for empirical frequency-band analysis of non-stationary time series.
- #'
- #' @keywords internal
- #' @useDynLib EFBA, .registration = TRUE
- #' @importFrom Rcpp evalCpp
- "_PACKAGE"
+#'
+#' Tools for empirical frequency-band analysis of non-stationary time series.
+#' These imports make base plotting and IO functions visible to the package,
+#' removing "no visible global function definition" NOTES.
+#'
+#' @keywords internal
+#' @useDynLib EFBA, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom graphics abline par plot.new
+#' @importFrom grDevices pdf dev.off gray rgb
+#' @importFrom utils read.csv
+"_PACKAGE"
